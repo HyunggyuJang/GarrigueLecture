04c8ac41c21db79cab38a0df5320efe3373d399d
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-06-14 12:04:23 +0900

    Fifth pass

--- Lecture9.v
+++ Lecture9.v
@@ -77,7 +77,16 @@ Section Nagoya2013.
     have Hm': m > 0 by apply ltnW.
     have -> : n.+3 ^ m - n.+3 = n.+2 ^ m - n.+2 + (n.+3 ^ m - 1 - n.+2 ^ m). admit.
     rewrite -IHn /Tm.
-    have -> : n.+3 ^ m - 1 - n.+2 ^ m = \sum_(1 <= k < m) 'C(m, k) * (Sk k n.+2 - Sk k n.+1). admit.
+    have -> : n.+3 ^ m - 1 - n.+2 ^ m = \sum_(1 <= k < m) 'C(m, k) * n.+2 ^ k. admit.
+    have -> : \sum_(1 <= k < m) 'C(m, k) * n.+2 ^ k = \sum_(1 <= k < m) 'C(m, k) * (Sk k n.+2 - Sk k n.+1).
+    {
+      apply: eq_bigr => i _.
+      congr (_ * _).
+      rewrite /Sk.
+      rewrite (@big_cat_nat _ _ _ n.+2) //=.
+      rewrite addKn.
+      by rewrite big_nat1.
+    }
     rewrite -big_split /=.
     apply: eq_bigr => i _.
     rewrite -mulnDr addnBA => //.
